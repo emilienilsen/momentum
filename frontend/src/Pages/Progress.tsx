5fd9a221d0027b7b187f932178540e5833f70c4a
Emilie <emilienilseen98@gmail.com>
2025-05-03 20:52:03 +0200

    Progress.tsx

--- frontend/src/Pages/Progress.tsx
+++ frontend/src/Pages/Progress.tsx
@@ -1,5 +1,5 @@
 export function Progress() {
     return (
-        <img src='../../public/momentum_logo.svg' className="mr-2 " alt='Logo'></img>
+        <img src='/momentum_logo.svg' className="mr-2 " alt='Logo'></img>
     )
 }
